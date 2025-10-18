558c29bc209480dcea724d1b37334e1c0f152c9c
ricky <splixx05@gmail.com>
2025-10-19 01:29:25 +0200

    update vars in plugin init

--- zsh-cloud-drives.zsh
+++ zsh-cloud-drives.zsh
@@ -1,8 +1,68 @@
-#!/bin/sh
+# -------------- Google Drive --------------------
+function cp-gdrive() {
+  local script_dir="${${(%):-%x}:A:h}"
+  local gdcp_script="$script_dir/gdscr/gdcp.sh"
+  
+  if [[ -n $TMUX ]]; then
+    tmux new-window -n google "$gdcp_script"
+  else
+    "$gdcp_script"
+  fi
+}
 
-FUNCTION="${ZSH:-$HOME/.zsh}/plugins/local/zsh-cloud-drives/functions"
+function mnt-gdrive() {
+  local script_dir="${${(%):-%x}:A:h}"
+  local gdmnt_script="$script_dir/gdscr/gdmnt.sh"
+  
+  if [[ -n $TMUX ]]; then
+    tmux new-window -n google "$gdmnt_script"
+  else
+"$gdmnt_script"
+  fi
+}
 
-# Load functions
-for file in $FUNCTION/*.zsh; do
-  [[ -f "$file" ]] && source "$file"
-done
+function up-gdrive() {
+  local script_dir="${${(%):-%x}:A:h}"
+  local gdup_script="$script_dir/gdscr/gdup.sh"
+  
+  if [[ -n $TMUX ]]; then
+    tmux new-window -n google "$gdup_script"
+  else
+"$gdup_script"
+  fi
+}
+
+# -------------- One Drive --------------------
+
+function cp-onedrive() {
+  local script_dir="${${(%):-%x}:A:h}"
+  local odcp_script="$script_dir/onescr/odcp.sh"
+
+  if [[ -n $TMUX ]]; then
+    tmux new-window -n onedrive "$odcp_script"
+  else
+    $OSRC/odcp.zsh
+  fi
+}
+
+function mnt-onedrive() {
+  local script_dir="${${(%):-%x}:A:h}"
+  local odmnt_script="$script_dir/onescr/odmnt.sh"
+  
+  if [[ -n $TMUX ]]; then
+    tmux new-window -n onedrive "$odmnt_script"
+  else
+"$odmnt_script"
+  fi
+}
+
+function up-onedrive() {
+  local script_dir="${${(%):-%x}:A:h}"
+  local odup_script="$script_dir/onescr/odup.sh"
+
+  if [[ -n $TMUX ]]; then
+    tmux new-window -n onedrive "$odup_script"
+  else
+"$odup_script"
+  fi
+}
